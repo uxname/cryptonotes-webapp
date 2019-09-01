2a0673e1e60b1e5e1434283e9a37ae1533ddf443
uxname <uxname@gmail.com>
2019-09-01 23:38:00 +0300

    Implemented dotenv

--- src/helper/apolloClient.js
+++ src/helper/apolloClient.js
@@ -1,7 +1,10 @@
 import ApolloClient from "apollo-boost";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000"
+    uri: process.env.REACT_APP_GRAPHQL_URL
 });
 
 export default client;
